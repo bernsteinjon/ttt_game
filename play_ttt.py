903f69a2e8cdc1b3cf3c1e7cbf1749ea961a1cc9
bernsteinjon <jbernstein@ias.edu>
2020-12-30 17:52:28 -0500

    added function to display game state

--- play_ttt.py
+++ play_ttt.py
@@ -11,3 +11,12 @@ gameBoard = {
     "8": "",
     "9": ""
 }
+
+def display_gameBoard(gameBoard):
+    print(" " + gameBoard["1"] + " | " + gameBoard["2"] + " | " + gameBoard["3"])
+    print("---+---+---")
+    print(" " + gameBoard["4"] + " | " + gameBoard["5"] + " | " + gameBoard["6"])
+    print("---+---+---")
+    print(" " + gameBoard["7"] + " | " + gameBoard["8"] + " | " + gameBoard["9"])
+
+display_gameBoard(gameBoard)
